dba242caabb722ab37620c906b27adeac15cd9b6
eduardasf <gabi.dudaa2102@gmail.com>
2023-11-17 17:40:45 -0300

    Add project files.

---
+++ atividade02/Classes.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularArea
+{
+
+    //***Calcular a área do círculo-----------------------------------------------------------------------
+    internal class Classes
+    {
+        public double raio;
+
+
+
+        public void TotalArea(double raio)
+        {
+            double resultado = 0;
+
+            resultado = Math.PI * Math.Pow(raio, 2);
+            Console.WriteLine($"\nA área do circulo é: {resultado:F2}");
+
+        }
+
+        public void PerimetroArea(double raio)
+        {
+            double resultado = 0;
+
+            resultado = 2 * Math.PI * raio;
+            Console.WriteLine($"\nO perimetro do circulo é: {resultado:F2}");
+        }
+    }
+
+
+    //***Calcular a área do retângulo------------------------------------------------------------------
+    internal class Retangulo
+    {
+        public int largura;
+        public int altura;
+
+
+        public void AreaRetangulo(int largura, int altura)
+        {
+            int resultado = 0;
+
+            resultado = largura * altura;
+            Console.Write("\nA área do retângulo é:" + resultado);
+
+        }
+
+        public void PerimetroRetangulo(int largura, int altura)
+        {
+            int resultado = 0;
+
+            resultado = 2*(largura + altura);
+            Console.Write("\nO perímetro do retângulo é:" + resultado);
+
+        }
+    }
+
+    //***Calcular a média do aluno------------------------------------------------------------------
+    internal class Aluno
+    {
+
+        public string nome;
+        public int matricula;
+        public double nota1;
+        public double nota2;
+        public double nota3;
+
+        public void MediaAluno(double nota1, double nota2, double nota3, string nome, int matricula)
+        {
+            double media;
+
+            media = (nota1 + nota2 + nota3) / 3;
+
+            Console.WriteLine("\n**INFORMAÇÕES DO ALUNO:**");
+
+            if(media < 6)
+            {
+                Console.WriteLine("Aluno: " + nome + "\nMatrícula:" + matricula + $"\nMédia:{media:F1}\nEstá: Reprovado");
+            }else
+            {
+                Console.WriteLine("Aluno: " + nome + "\nMatrícula:" + matricula + $"\nMédia:{media:F1}\nEstá: Aprovado");
+            }
+
+        }
+
+
+    }
+
+    //***Calcular o fatorial do número------------------------------------------------------------------
+
+    internal class Fatorial
+    {
+        public int numero;
+
+        public void CalcularFatoria(int numero)
+        {
+            int resultado = 1;
+
+            for (int i = 1; i <= numero; i++)
+            {
+                resultado *= i;
+            }
+
+            Console.WriteLine($"O fatorial de {numero} é {resultado}");
+        }
+    }
+
+    //***Sorteio------------------------------------------------------------------
+    class Sorteio
+    {
+        private int numeroSorteado;
+        private int tentativas;
+
+        public Sorteio()
+        {
+  
+            Random random = new Random();
+            numeroSorteado = random.Next(11);
+
+            tentativas = 0;
+        }
+
+        public int NumeroSorteado
+        {
+            get { return numeroSorteado; }
+        }
+
+        public void RealizarSorteio(int numeroUsuario)
+        {
+            tentativas++;
+
+            if (numeroUsuario == numeroSorteado)
+            {
+                Console.WriteLine($"Parabéns! Você acertou o número {numeroSorteado} em {tentativas} tentativa(s).");
+            }
+            else
+            {
+                Console.WriteLine($"Você errou. Tente novamente.");
+            }
+        }
+    }
+}
---
+++ atividade02/Program.cs
@@ -0,0 +1,84 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CalcularArea;
+using System.ComponentModel;
+
+class atividade02
+{
+    static void Main()
+    {
+
+        /**Questão 01---------------------------------------------------------------------------------------------
+        Circulo circulo = new Circulo();
+        Console.Write("Digite o valor do raio:");
+        circulo.raio = double.Parse(Console.ReadLine());
+
+        circulo.TotalArea(circulo.raio);
+        circulo.PerimetroArea(circulo.raio);**/
+
+        /**Questão 02----------------------------------------------------------------------------------------------
+
+        Retangulo r1 = new Retangulo();
+        Console.Write("Digite o valor da largura:");
+        r1.largura = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor da altura:");
+        r1.altura = int.Parse(Console.ReadLine());
+
+        r1.AreaRetangulo(r1.largura, r1.altura);
+        r1.PerimetroRetangulo(r1.largura, r1.altura);**/
+
+        /**Questão 03---------------------------------------------------------------------------------------------
+
+        Aluno a1 = new Aluno();
+
+        Console.Write("Digite o nome do aluno:");
+        a1.nome = Console.ReadLine();
+
+        Console.Write("Digite a matrícula do aluno:");
+        a1.matricula = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite a primeira nota:");
+        a1.nota1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a segunda nota:");
+        a1.nota2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a terceira nota:");
+        a1.nota3 = double.Parse(Console.ReadLine());
+
+        a1.MediaAluno(a1.nota1, a1.nota2, a1.nota3, a1.nome, a1.matricula);**/
+
+
+        /**Questão 04---------------------------------------------------------------------------------------------
+
+        Fatorial f1 = new Fatorial();
+        Console.Write("Digite um número para saber seu fatorial:");
+        f1.numero = int.Parse(Console.ReadLine());
+
+        f1.CalcularFatoria(f1.numero);**/
+
+        //Questão 05-----------------------------------------------------------------------------------------------
+
+     
+            Sorteio sorteio = new Sorteio();
+            int numeroUsuario;
+
+            do
+            {
+                Console.Write("Digite um número de 0 a 10: ");
+                string inputNumero = Console.ReadLine();
+
+                if (int.TryParse(inputNumero, out numeroUsuario) && numeroUsuario >= 0 && numeroUsuario <= 10)
+                {
+                    sorteio.RealizarSorteio(numeroUsuario);
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida. Certifique-se de digitar um número válido entre 0 e 10.");
+                }
+
+            } while (numeroUsuario != sorteio.NumeroSorteado);
+        }
+
+}
